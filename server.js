069e70c9174903ecf3c72e31d5499b8885eaa779
Rolando <rolando0902@gmail.com>
2020-04-14 19:28:05 -0400

    change port

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const http = require('https');
 const app = require('./app');
 
 const server = http.createServer(app);
-server.listen(3000, 'https://cubapack-service.herokuapp.com');
+server.listen(8080);

5812b6b645c0d8bc73f1fdea8240d086925568de
Rolando <rolando0902@gmail.com>
2020-04-14 19:22:55 -0400

    fixing protocol

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http');
+const http = require('https');
 const app = require('./app');
 
 const server = http.createServer(app);
-server.listen(3000);
+server.listen(3000, 'https://cubapack-service.herokuapp.com');

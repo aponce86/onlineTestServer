5911335a45e5b372c5a8d155f8150fb6a24b72c7
Rolando <rolando0902@gmail.com>
2020-04-14 21:36:23 -0400

    a

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     console.log('creating server');
     res.end('tito loko');
 });
 
-//console.log(process.env);
-
-//const hostname = 'https://cubapack-service.herokuapp.com/';
-
-const port = process.env.PORT || 5000;
 
 console.log(port);
 
-server.listen(port, ()=> {
-    console.log('listening here');
-});
+server.listen(port);
 
 /*
 const http = require('https');

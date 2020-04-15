f253369c54640b5880bc4d4a8098f47a37038168
Rolando <rolando0902@gmail.com>
2020-04-14 21:25:16 -0400

    fixing port to deploy

--- server.js
+++ server.js
@@ -1,24 +1,7 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    console.log('creating server');
-    res.end('tito loko');
-});
-
-//console.log(process.env);
-
-//const hostname = 'https://cubapack-service.herokuapp.com/';
-
-const port = process.env.PORT || 5000;
-
-server.listen(port, ()=> {
-    console.log('listening here');
-});
-
-/*
 const http = require('https');
 const app = require('./app');
 
+const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
-server.listen(8080);
-*/
+server.listen(port);

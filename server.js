339ea2a4ee795e27bf9d525e292a940f34decdb0
Rolando <rolando0902@gmail.com>
2020-04-14 21:28:25 -0400

    a

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
+
+const server = http.createServer((req, res) => {
+    res.end('lololololol');
+});
+
 server.listen(port);
